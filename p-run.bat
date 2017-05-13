37918f80d2e9bf81e25fe6cae1db7da2c28bf201
Ziv Harpaz <ziv_harpaz@yahoo.com>
2017-05-13 09:41:52 +0300

    Use subtest to run the different test cases

---
+++ p-run.bat
@@ -0,0 +1,3 @@
+echo off
+set PYTHONPATH=C:\Users\Ziv\.p2\pool\plugins\org.python.pydev_4.4.0.201510052309\pysrc\pydev_sitecustomize;D:\DATA\Ziv\Programming\SRTCorrecter\srtFix\src;D:\DATA\Ziv\Programming\Python\DLLs;D:\DATA\Ziv\Programming\Python\lib;D:\DATA\Ziv\Programming\Python;D:\DATA\Ziv\Programming\Python\lib\site-packages;h:\H-DATA\OneDrive\Ziv\Programming\SRTCorrecter\srtFix\src\util
+python %*
--- srtFix/src/util/testChecksum.py
+++ srtFix/src/util/testChecksum.py
@@ -3,34 +3,36 @@ Created on May 11, 2017
 Test files taken from http://trac.opensubtitles.org/projects/opensubtitles/wiki/HashSourceCodes
 @author: Ziv
 '''
+import inspect
 import os
 import unittest
 
 import checksum
 
 
-class Test(unittest.TestCase):
-
+class TestCheckSum(unittest.TestCase):
 
     def setUp(self):
-        pass
-
+      TestCheckSum.tstDataDir=os.path.dirname(inspect.getfile(TestCheckSum))+"\\test-data"
+      pass
 
     def tearDown(self):
-        pass
-
+      pass
 
     def testFiles(self):
+      ''' runs a list of test cases on different files'''
       tData=({'fl':"not-exist",'rc':"IOError"},
-             {'fl':os.environ['PROJECT_LOC']+"\\test-data\\test-eng.srt",  'rc':"SizeError"},
-             {'fl':os.environ['PROJECT_LOC']+"\\test-data\\breakdance.avi",'rc':'8e245d9679d31e12'}
-             #''{'fl':os.environ['PROJECT_LOC']+"\\test-data\\dummy.bin",     'rc':'61f7751fc2a72bfb'}
+             {'fl':TestCheckSum.tstDataDir+"\\test-eng.srt",  'rc':"SizeError"},
+             {'fl':TestCheckSum.tstDataDir+"\\breakdance.avi",'rc':'8e245d9679d31e12'}
+             #''{'fl':TestCheckSum.tstDataDir+"\\test-data\\dummy.bin",     'rc':'61f7751fc2a72bfb'}
              # commented out due to size of file (4G).
              )
       for td in tData:
-        self.testFile(td['fl'],td['rc'])
+        with self.subTest(td):
+          self.ChkFile(td['fl'],td['rc'])
 
-    def testFile(self, fl, rc):
+    def ChkFile(self, fl, rc):
+      '''Perform a single test case'''
       try:
         res=checksum.hashFile(fl)
         self.assertTrue(rc == res,
@@ -51,8 +53,4 @@ class Test(unittest.TestCase):
 
 if __name__=="__main__":
     # import sys;sys.argv = ['', 'Test.testName']
-    suite = unittest.TestSuite()
-    suite.addTest(Test('testFiles'))
-    unittest.TextTestRunner().run(suite)
-    
-    # unittest.main()
+    unittest.main()
